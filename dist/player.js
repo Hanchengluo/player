348659bc72a7757bd93e97944ea8a92ebab00822
switer <guankaishe@gmail.com>
2015-10-27 18:10:27 +0800

    poster

--- dist/player.js
+++ dist/player.js
@@ -1382,6 +1382,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	var util = __webpack_require__(4)
 	var Video = __webpack_require__(5)
 	var Control = __webpack_require__(7)
+	var Poster = __webpack_require__(9)
 	var proto = Player.prototype
 
 	function Player (opts) {
@@ -1423,17 +1424,28 @@ return /******/ (function(modules) { // webpackBootstrap
 		})
 		$video.$appendTo($view)
 
+		if (opts.ui.control) {
+			/**
+			 * Video component
+			 */
+			var $control = this.$refs.control = new Control({
+				parent: $view,
+				replace: true,
+				data: {}
+			})
+			$control.$appendTo($view)
+		}
 		/**
-		 * Video component
+		 * Poster component
 		 */
-		var $control = this.$refs.control = new Control({
+		var $poster = this.$refs.poster = new Poster({
 			parent: $view,
 			replace: true,
 			data: {
-				autoplay: false
+				poster: opts.ui.poster || ''
 			}
 		})
-		$control.$appendTo($view)
+		$poster.$appendTo($view)
 
 	}
 	/**
@@ -1605,6 +1617,37 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ function(module, exports) {
 
 	module.exports = "<div class=\"tvp_controls\">\n\t<div class=\"tvp_button tvp_playpause_button tvp_play\" \n\t\tr-on=\"{click: _onPlay}\"\n\t\tr-class=\"{\n\t\t\ttvp_play: !play;\n\t\t\ttvp_pause: play;\n\t\t}\"\n\t>\n\t\t<button type=\"button\" title=\"播放/暂停\"><span class=\"tvp_btn_value\">播放</span></button>\n\t</div>\n\t<div class=\"tvp_time_rail\">\n\t\t<span class=\"tvp_time_total\">\n\t\t\t<span class=\"tvp_time_loaded\" style=\"width: 100%;\"></span>\n\t\t\t<span class=\"tvp_time_current\" style=\"width: 0px;\"><span class=\"tvp_time_handle\"></span></span>\n\t\t</span>\n\t\t<span class=\"tvp_time_panel\">\n\t\t\t<span class=\"tvp_time_panel_current\">00:00</span>\n\t\t\t<span class=\"tvp_time_panel_split\">/</span>\n\t\t\t<span class=\"tvp_time_panel_total\">00:15</span>           \n\t\t</span>  \n\t</div>\n\t<div class=\"tvp_barrage_switch tvp_none\">\n\t\t<div class=\"tvp_btn_barrage\" data-role=\"tvp-bullet-switch\">\n\t\t\t<div class=\"tvp_btn_value\">弹</div>\n\t\t</div>\n\t</div>\n\t<div class=\"tvp_button tvp_volume tvp_none\">\n\t\t<div class=\"tvp_btn_volume\"><div class=\"tvp_icon_volume\"></div></div>\n\t\t<div class=\"tvp_volume_slider\">\n\t\t\t<div class=\"tvp_volume_range\">\n\t\t\t\t<div class=\"tvp_volume_range_current\" style=\"height:50%\"><div class=\"tvp_volume_handle\"></div></div>\n\t\t\t</div>\n\t\t</div>\n\t</div>\n\t<div class=\"tvp_button tvp_definition _tvp_definition_ tvp_none\">\n\t\t<div class=\"tvp_definition_button\"><span>清晰度</span></div>\n\t<div class=\"tvp_definition_list\"></div>  \n\t</div>\n\t<div class=\"tvp_button tvp_fullscreen_button tvp_fullscreen\">\n\t\t<button type=\"button\" title=\"切换全屏\"><span class=\"tvp_btn_value\">全屏</span></button>  \n\t</div>\n\t<span class=\"tvp_time_handel_hint\" style=\"display:none\"></span>\n</div>";
+
+/***/ },
+/* 9 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	module.exports = Real.create({
+		data: function () {
+			return {
+				show: true,
+				poster: ''
+			}
+		},
+		template: __webpack_require__(10),
+		ready: function () {
+			this.$listen('play', this.hide)
+		},
+		methods: {
+			hide: function () {
+				this.$data.show = false
+				this.$update()
+			}
+		}
+	})
+
+/***/ },
+/* 10 */
+/***/ function(module, exports) {
+
+	module.exports = "<div class=\"tvp_overlay_poster\" r-show=\"{show}\">\n\t<img class=\"tvp_poster_img\" \n\t\tr-attr=\"{src: poster}\"\n\t/>\n</div>";
 
 /***/ }
 /******/ ])
---
+++ lib/comps/poster/poster.tpl
@@ -0,0 +1,5 @@
+<div class="tvp_overlay_poster" r-show="{show}">
+	<img class="tvp_poster_img" 
+		r-attr="{src: poster}"
+	/>
+</div>
